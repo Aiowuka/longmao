3dda6fb6a3fee8ec73d676e6e32b805e3086be85
Aiowuka <1304889335@qq.com>
2026-09-20 22:22:25 +0800

    chore: pin installer to consolidated validated head

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 REPO='Aiowuka/longmao'
-DEFAULT_REF='6d0d52ef2124c4142a9877bb6915b916e98a0bb9'
+DEFAULT_REF='b5e9bacc2d4a5e1c729d314ba2880c18caad2213'
 REF="${LONGMAO_REF:-$DEFAULT_REF}"
 ARCHIVE_URL="https://github.com/$REPO/archive/$REF.tar.gz"
 
